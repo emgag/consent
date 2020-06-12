dc416682f2b548ed4fbdf003599587c46d1a5635
Matthias Blaser <git@mooch.ch>
2020-06-12 11:21:13 +0200

    Remove oil.js and cookiebot implementations

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,7 @@ var Encore = require('@symfony/webpack-encore');
 Encore
     .setOutputPath('dist/')
     .setPublicPath('/dist')
-    .addEntry('cookiebot-bundle', './src/cookiebot-bundle.js')
     .addEntry('noop-bundle', './src/noop-bundle.js')
-    .addEntry('oiljs-bundle', './src/oiljs-bundle.js')
-    .copyFiles({
-        from: './node_modules/@ideasio/oil.js/release/current',
-        to: 'external/[name].[ext]',
-        pattern: /RELEASE/
-    })
     .disableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
