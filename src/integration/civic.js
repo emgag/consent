7364f1695d0a98ada82dc4fc9406e36b736a0895
Matthias Blaser <git@mooch.ch>
2020-06-26 12:17:57 +0200

    Handle CMP close events

--- src/integration/civic.js
+++ src/integration/civic.js
@@ -8,58 +8,108 @@ class Consent_Civic {
         this.config = config;
     }
 
-    init(consent, callback){
+    init(consent, callback) {
         this.open = false;
 
         __tcfapi('addEventListener', 2, (tcData, success) => {
-            if(!success) return false
+            if (!success) return false
             consent.log('TCM API', tcData.eventStatus, tcData);
 
-            switch(tcData.eventStatus){
+            const commit = (tcData) => {
+                if (tcData.hasOwnProperty('purpose') && tcData.hasOwnProperty('publisher')) {
+                    callback(
+                        {
+                            'standard': this.annotateStandardConsents(tcData.purpose.consents),
+                            'special': tcData.publisher.customPurpose.consents
+                        },
+                        tcData.tcString
+                    );
+                }
+            }
+
+            switch (tcData.eventStatus) {
                 case 'useractioncomplete':
-                    // fallthrough
+                // fallthrough
                 case 'tcloaded':
-                    if(this.open){
+                    if (this.open) {
                         consent.call('dialog_close');
                         this.open = false
                     }
 
-                    if(tcData.hasOwnProperty('purpose') && tcData.hasOwnProperty('publisher')) {
-                        callback(
-                            {
-                                'standard': this.annotateStandardConsents(tcData.purpose.consents),
-                                'special': tcData.publisher.customPurpose.consents
-                            },
-                            tcData.tcString
-                        );
-                    }
+                    commit(tcData);
 
                     break;
 
                 case 'cmpuishown':
+                    if (!this.open) {
+                        consent.call('dialog_open');
+                    }
+
                     this.open = true;
-                    consent.call('dialog_open');
+
+                    const closeHandler = e => {
+                        this.open = false;
+                        consent.call('dialog_close');
+
+                        // consent is commited now and can be accessed, but not with provided
+                        // tcData object, needs to be requested again
+                        __tcfapi('getTCData', 2, (tcData, success) => { commit((tcData)) })
+                    }
+
+                    // every change in the UI triggers a cmpuishown event, but closing the UI using
+                    // either the close icon, ESC key or the save preferences button does not trigger
+                    // an event.
+                    window.addEventListener(
+                        'keydown',
+                        e => {
+                            let isEscape = false;
+                            if ("key" in e) {
+                                isEscape = (e.key === "Escape" || e.key === "Esc");
+                            } else {
+                                isEscape = (e.keyCode === 27);
+                            }
+
+                            if (isEscape) {
+                                closeHandler(e)
+                            }
+                        },
+                        {once: true}
+                    );
+
+                    document.getElementById('ccc-dismiss-button').addEventListener(
+                        'click',
+                        closeHandler,
+                        {once: true}
+                    );
+
+                    document.getElementById('ccc-close').addEventListener(
+                        'click',
+                        closeHandler,
+                        {once: true}
+                    );
+
+
                     break;
             }
         })
     }
 
-    annotateStandardConsents(consents){
+    annotateStandardConsents(consents) {
         const c = {}
 
-        for(let i = 1; i <= 10; i++){
+        for (let i = 1; i <= 10; i++) {
             c[i] = consents.hasOwnProperty(i) ? consents[i] : false;
         }
 
         return c
     }
 
-    optOut(){
+    optOut() {
         CookieControl.delete('CookieControl');
         CookieControl.delete('CookieControlTC');
     }
 
-    showUi(){
+    showUi() {
         CookieControl.open()
     }
 
