f3b4276008edad5794e010588be0a1d4f633539c
Matthias Blaser <git@mooch.ch>
2020-09-21 12:34:39 +0200

    Removed close handling hacks fixed by upstream

--- src/integration/civic.js
+++ src/integration/civic.js
@@ -46,49 +46,6 @@ class Consent_Civic {
                     }
 
                     this.open = true;
-
-                    const closeHandler = e => {
-                        this.open = false;
-                        consent.call('dialog_close');
-
-                        // consent is commited now and can be accessed, but not with provided
-                        // tcData object, needs to be requested again
-                        __tcfapi('getTCData', 2, (tcData, success) => { commit((tcData)) })
-                    }
-
-                    // every change in the UI triggers a cmpuishown event, but closing the UI using
-                    // either the close icon, ESC key or the save preferences button does not trigger
-                    // an event.
-                    window.addEventListener(
-                        'keydown',
-                        e => {
-                            let isEscape = false;
-                            if ("key" in e) {
-                                isEscape = (e.key === "Escape" || e.key === "Esc");
-                            } else {
-                                isEscape = (e.keyCode === 27);
-                            }
-
-                            if (isEscape) {
-                                closeHandler(e)
-                            }
-                        },
-                        {once: true}
-                    );
-
-                    document.getElementById('ccc-dismiss-button').addEventListener(
-                        'click',
-                        closeHandler,
-                        {once: true}
-                    );
-
-                    document.getElementById('ccc-close').addEventListener(
-                        'click',
-                        closeHandler,
-                        {once: true}
-                    );
-
-
                     break;
             }
         })
