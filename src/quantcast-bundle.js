c1742820f4c8b72dee56351f09916a10d696bb13
Matthias Blaser <git@mooch.ch>
2020-12-02 14:48:41 +0100

    Remove noop from bundle

--- src/quantcast-bundle.js
+++ src/quantcast-bundle.js
@@ -5,9 +5,6 @@
 import Consent_Quantcast from "./integration/quantcast";
 global.Consent_Quantcast = Consent_Quantcast;
 
-import Consent_Noop from "./integration/noop";
-global.Consent_Noop = Consent_Noop;
-
 import Consent from "./consent/shim";
 global.Consent = Consent;
 
